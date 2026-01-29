fc85075329cf7f648f828f0a55f81fae15da73d5
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-01-29 13:54:49 +0100

    Cambios para despliegue de la aplicación

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { GifsApp } from "./GifsApp";
-import { MyCounterApp } from "./counter/components/MyCounterApp";
+// import { MyCounterApp } from "./counter/components/MyCounterApp";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
