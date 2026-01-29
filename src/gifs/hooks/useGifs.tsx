bacdfafaffdc9df439734c032a0f8f8a266b5de4
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-01-29 13:25:34 +0100

    Añandiendo cache al hook personalizado useGifs

--- src/gifs/hooks/useGifs.tsx
+++ src/gifs/hooks/useGifs.tsx
@@ -1,14 +1,32 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 import { getGifsByQuery } from "../actions/get-gifs-by-query.action";
 import type { Gif } from "../interfaces/gif.interface";
+
+// const gifsCache: Record<string, Gif[]> = {};
 
 export const useGifs = () => {
   const [gifs, setGifs] = useState<Gif[]>([]);
   const [previousTerms, setPreviousTerms] = useState<string[]>([]);
 
+  // useRef: Referencia mutable que no causa re-render
+  const gifsCache = useRef<Record<string, Gif[]>>({})
+
   const handleTermClicked = async (term: string) => {
     // console.log(term);
+
+    // Comprobar caché con gifsCache fuera de useGifs
+    // if (gifsCache[term]) {
+    //   setGifs(gifsCache[term]);
+    //   return;
+    // }
+
+    // Comprobar caché con gifsCache con useRef
+    if (gifsCache.current[term]) {
+      setGifs(gifsCache.current[term]);
+      return;
+    }
+
     const gifs = await getGifsByQuery(term);
     setGifs(gifs);
   };
@@ -28,7 +46,13 @@ export const useGifs = () => {
     const gifs = await getGifsByQuery(query);
     // console.log(gifs);
     setGifs(gifs);
-  };
+
+    // Gestionando caché con variable externa
+    // gifsCache[query] = gifs;
+    // Gestionando caché con useRef
+    gifsCache.current[query] = gifs;
+    console.log(gifsCache);
+  };;
 
   return {
     // Values
